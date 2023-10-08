e6f728dfd5c29235c83b801eed42f04880a09ef3
Luigi <Luigialbertosuarezvera@gmail.com>
2023-10-07 22:51:10 -0500

    barra color secundario

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,7 @@
 const menuToggle = document.querySelector('.menu-toggle');
-        const menu = document.querySelector('.menu');
+const menu = document.querySelector('.menu');
 
-        menuToggle.addEventListener('click', () => {
-            menu.classList.toggle('active');
-            if (menu.classList.contains('active')) {
-                menu.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
-                menu.style.backdropFilter = 'blur(10px)';
-            } else {
-                menu.style.backgroundColor = 'transparent'; // Restablece el fondo cuando se cierra el menú
-            }
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
 });
+
