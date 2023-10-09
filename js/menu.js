6f83aa407cc57759fc73523e66f5d79926e39ca4
Luigi <Luigialbertosuarezvera@gmail.com>
2023-10-08 21:31:36 -0500

    cerrado automatico de menu

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,14 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menu__item'); // Obtén todos los elementos del menú
 
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
 
+// Agregar evento de clic a cada elemento del menú
+menuItems.forEach((menuItem) => {
+    menuItem.addEventListener('click', () => {
+        menu.classList.remove('active'); // Cierra el menú al hacer clic en un elemento del menú
+    });
+});
